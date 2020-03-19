f0f48f3f1b208b3ca1d54a12595a903d013d81d9
Avi Shukron <avraham.shukron@gmail.com>
2020-03-20 00:42:39 +0200

    added tests and conda env file
    Signed-off-by: Avi Shukron <avraham.shukron@gmail.com>

---
+++ test/downstream/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#include <fact/fact.hpp>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+	cout << factorial(4) << endl;
+	return 0;
+}
---
+++ test/tests.cpp
@@ -0,0 +1,21 @@
+#include <CppUTest/TestHarness.h>
+#include <CppUTest/CommandLineTestRunner.h>
+
+
+#include <fact/fact.hpp>
+
+TEST_GROUP(FactTests)
+{};
+
+TEST(FactTests, FactorialCorrectness)
+{
+  CHECK_EQUAL(factorial(1), 1);
+  CHECK_EQUAL(factorial(2), 2);
+  CHECK_EQUAL(factorial(3), 6);
+  CHECK_EQUAL(factorial(10), 3628800);
+}
+
+int main(int argc, char** argv)
+{
+    return CommandLineTestRunner::RunAllTests(argc, argv);
+}
